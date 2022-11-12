6eb8cb1b0b0f7593185e942dc553c84b183ff12c
swxfll <swxfll@outlook.com>
2022-11-13 00:05:37 +0800

    捕获异常修改

--- 10-exception/main.go
+++ 10-exception/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
+// 捕获异常
 func test() {
 	// 使用defer+recover来捕获错误: defer后加上匿名函数的调用
 	defer func() {
@@ -19,7 +23,32 @@ func test() {
 	fmt.Println(result)
 }
 
+// 自定义异常
+func test2() (er error) {
+	num1 := 10
+	num2 := 0
+
+	if num2 == 0 {
+		//抛出自定义异常
+		return errors.New("除数不能为0")
+	}
+
+	result := num1 / num2
+	fmt.Println(result)
+	return nil
+}
+
 func main() {
+	// 1 捕获异常
 	test()
 	fmt.Println("hello")
+
+	// 2 自定义异常
+	err := test2()
+	if err != nil {
+		fmt.Println(err) // 输出 除数不能为0
+		//panic(err)       // 停止当前Go程序(退出main函数),不继续往下执行
+	}
+
+	fmt.Println("main")
 }
